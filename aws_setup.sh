9d94eca3ffad12de91834d5bfc102123b18e4004
t-welton <49294609+t-welton@users.noreply.github.com>
2023-10-16 11:35:46 +0800

    Add files via upload

--- aws_setup.sh
+++ aws_setup.sh
@@ -1,23 +1,22 @@
-# setup env - t3a.2xlarge / EBS
+# setup environment on aws t3a.2xlarge with 1000GiB EBS
 
 sudo yum update
 sudo yum -y install git
+git clone https://github.com/hakyimlab/MetaXcan
+git clone https://github.com/hakyimlab/summary-gwas-imputation
+git clone https://github.com/t-welton/Nigrostriatal_Imputation
 
 wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh
 sudo bash Miniconda3-latest-Linux-x86_64.sh -b -p /opt/miniconda3
 export PATH=$PATH:/opt/miniconda3/bin
-
-git clone https://github.com/hakyimlab/MetaXcan
-git clone https://github.com/hakyimlab/summary-gwas-imputation
-
 conda config --remove channels defaults
 conda config --add channels conda-forge
-
 conda env create -f MetaXcan/software/conda_env.yaml
 conda init bash
 bash
 
-# download
+# download data
+
 mkdir ~/data
 wget -P ~/data https://www.fmrib.ox.ac.uk/ukbiobank/gwas_resources/QSMandT2s_IDPs.tar.gz	#10GB
 wget -P ~/data https://www.fmrib.ox.ac.uk/ukbiobank/gwas_resources/QSMandT2s_IDPs_repro.tar.gz	#10GB
@@ -29,10 +28,11 @@ wget -P ~/data https://open.win.ox.ac.uk/ukbiobank/big40/release2/stats/1442.txt
 wget -P ~/data https://open.win.ox.ac.uk/ukbiobank/big40/release2/repro/1442.txt.gz
 wget -P ~/data https://open.win.ox.ac.uk/ukbiobank/big40/release2/stats/1443.txt.gz
 wget -P ~/data https://open.win.ox.ac.uk/ukbiobank/big40/release2/repro/1443.txt.gz
-
 tar –xvzf ~/data/QSMandT2s_IDPs.tar.gz -C ~/data
 tar –xvzf ~/data/QSMandT2s_IDPs_repro.tar.gz  -C ~/data
 
-
+# download gtex models
+# download liftover chainfile
+# download 1000g reference data
 
 
